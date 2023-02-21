5b4beec57a1e7ad264e262e959f141e447310a01
oyindoubra <doubbynath@gmail.com>
2023-02-20 17:13:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a new line"
  *
- * Return 0.
+ * Return: Always 0.
  */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
 
-    return(0);
+    return (0);
 }
