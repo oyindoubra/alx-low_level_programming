4907852390228e3ba5a8702a5e0ad312e3d08763
oyindoubra <doubbynath@gmail.com>
2023-05-24 14:34:16 -0700

    iii

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned long int n);
+int get_endianness(void);
+
+
+#endif
